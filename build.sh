a42992a94bfbd576a3caa030607807d3bafd076c
taozha2 <tao2.zhang@intel.com>
2025-04-14 00:31:42 -0700

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cp ../bui script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 rm -rf ${script_dir}/.git/hooks/pre-commit
 rm -rf ${script_dir}/build && mkdir ${script_dir}/build && cd ${script_dir}/build
 
