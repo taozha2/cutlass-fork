2e0b65dafa6b22122077fc414828ba148f2ba433
taozha2 <tao2.zhang@intel.com>
2025-04-13 23:48:49 -0700

    update

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ unset IGC_VISAOptions
 # ================= compiler / driver =================
 # https://github.com/intel/llvm/releases/tag/nightly-2024-05-16
 # https://ubit-gfx.intel.com/build/19168301/artifacts
-sycl_compiler_path=/opt/cutlass/compiler/1008/
+sycl_compiler_path=/opt/cutlass/compiler/20250414/
 gpu_driver_path=/opt/cutlass/gpu_driver/gfx-driver-ci-comp_igc-29010/extract/
 export CPATH=$sycl_compiler_path:$sycl_compiler_path/include/:$sycl_compiler_path/include/sycl/
 export LIBRARY_PATH=$gpu_driver_path/usr/lib/x86_64-linux-gnu/:$sycl_compiler_path/lib/
