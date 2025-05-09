50e8a1592cf0dd32b00e4012d3dd461d83769686
taozha2 <tao2.zhang@intel.com>
2025-05-09 02:23:35 -0700

    update

--- examples/sycl/int4/pvc_gemm_int4_quantization.cpp
+++ examples/sycl/int4/pvc_gemm_int4_quantization.cpp
@@ -616,12 +616,12 @@ int main(int argc, const char** argv)
   using ElementScale = MmaType;
 
   // Note: XE_2D_U18x32x32_LD_N is incompatible with our bf16 MMA atoms
-  using GmemTiledCopyA = XE_2D_U4x16x64_LD_NN;
-  using GmemTiledCopyB = XE_2D_U16x32x16_LD_N;
+  using GmemTiledCopyA = XE_2D_U4x32x32_LD_NN;
+  using GmemTiledCopyB = XE_2D_U16x32x32_LD_N;
   static_assert(sizeof(ElementInputA) == 1, "ElementA width must match GmemTiledCopyA U8");
 
   // Workgroup-level tile
-  using TileShape = Shape<_256, _256, _16>;
+  using TileShape = Shape<_512, _128, _32>;
 
   using TiledMma =
       typename TiledMMAHelper<MMA_Atom<XE_8x16x16_F32F16F16F32_TT>, Layout<TileShape>,
