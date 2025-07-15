cb4b4933d80a44bf6d109c1b2f1f911f8b8fb57f
taozha2 <tao2.zhang@intel.com>
2025-07-15 15:19:13 +0800

    update

--- examples/sycl/02_bmg_gemm_mixed_dtype/02_bmg_gemm_f16_u4_s8.cpp
+++ examples/sycl/02_bmg_gemm_mixed_dtype/02_bmg_gemm_f16_u4_s8.cpp
@@ -83,9 +83,6 @@ enum GemmMode {
   ConvertAndScaleWithZeroPoint
 };
 
-using MmaType = cutlass::half_t;
-using QuantType = uint4_t;
-
 // Command line options parsing
 struct Options {
 
@@ -273,7 +270,7 @@ struct ExampleRunner {
             FusionCallBacks,
             XE_2D_U32x8x16_LD_N,
             void, void,
-            XE_2D_U16x8x16_ST_N,
+            XE_2D_U8x8x16_ST_N,
             void, void>;
 
     // Mainloop
@@ -707,7 +704,7 @@ int main(int argc, const char** argv) {
   using ElementComputeEpilogue = int32_t;  // <- data type of epilogue operations
   using ElementInputA = half_t;            // <- data type of elements in input matrix A
   using ElementInputB = uint4_t;           // <- data type of elements in input matrix B
-  using ElementOutput = half_t;            // <- data type of elements in output matrix D
+  using ElementOutput = int8_t;            // <- data type of elements in output matrix D
 
   using LayoutA = cutlass::layout::RowMajor;
   using LayoutB = cutlass::layout::ColumnMajor;
@@ -715,7 +712,7 @@ int main(int argc, const char** argv) {
   using LayoutD = cutlass::layout::RowMajor;
 
   using ElementZero = int4_t;
-  using ElementScale = half_t;
+  using ElementScale = ElementInputA;
 
   using StrideScale = cute::Stride<_1, int64_t, int64_t>;
   using StrideZero = cute::Stride<_8, cute::Stride<_1, int64_t>, int64_t>; // int4_t zero point packed 8 elements along K dimension and then along M dimension
@@ -749,7 +746,7 @@ int main(int argc, const char** argv) {
           FusionCallBacks,
           XE_2D_U32x8x16_LD_N,
           void, void,
-          XE_2D_U16x8x16_ST_N,
+          XE_2D_U8x8x16_ST_N,
           void, void>;
 
   // Use the helpers to avoid template arg repetition
